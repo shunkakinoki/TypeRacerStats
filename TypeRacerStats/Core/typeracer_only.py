baf2bdf2bf0442c8488a82a6ecf2fb4c813944b8
Eugene Lee <e6f4e37l@gmail.com>
2021-01-04 15:46:48 +0900

    country codes

--- TypeRacerStats/Core/typeracer_only.py
+++ TypeRacerStats/Core/typeracer_only.py
@@ -1,14 +1,15 @@
 import asyncio
 import datetime
 import json
+import re 
 import sqlite3
 import sys
 from bs4 import BeautifulSoup
 import discord
 from discord.ext import commands, tasks
 sys.path.insert(0, '')
-from TypeRacerStats.config import BOT_OWNER_IDS, BLANK_FLAG
-from TypeRacerStats.file_paths import DATABASE_PATH, TYPERACER_RECORDS_JSON
+from TypeRacerStats.config import BOT_OWNER_IDS, BLANK_FLAG, HELP_IMG, TR_INFO
+from TypeRacerStats.file_paths import DATABASE_PATH, TYPERACER_RECORDS_JSON, COUNTRY_CODES
 from TypeRacerStats.Core.Common.accounts import check_banned_status
 from TypeRacerStats.Core.Common.formatting import escape_sequence
 from TypeRacerStats.Core.Common.data import fetch_data
@@ -48,6 +49,8 @@ class TypeRacerOnly(commands.Cog):
         if 't!tg train' in message.content:
             if message.author.id == 476016555981930526: #pasta's Discord ID
                 await message.channel.send(content = f"<@{message.author.id}>, this is NOT bot usage.")
+            elif message.author.id == 742267194443956334: #TypeRacerStat's Discord ID
+                await message.channel.send(content = '<a:pasta_200_iq_patch:794905370011107328>')
             else:
                 embed = discord.Embed(title = 'Training Success!',
                                       color = discord.Color(0x4B9F6C),
@@ -136,7 +139,6 @@ class TypeRacerOnly(commands.Cog):
         with open(TYPERACER_RECORDS_JSON, 'w') as jsonfile:
             json.dump(updated_file, jsonfile, indent = 4)
 
-        self.update_init_variables()
         try:
             await self.edit_record_messages()
         except NotImplementedError:
@@ -190,19 +192,30 @@ class TypeRacerOnly(commands.Cog):
         self.country_tally = dict()
         self.user_tally = dict()
 
+        faq_thumbnail = HELP_IMG
+        speed_thumbnail, speed_color = 'https://i.imgur.com/bXAjl4C.png', 0x001359
+        three_hundred_thumbnail, three_hundred_color = 'https://i.imgur.com/i8kXn3K.png', 0x1B0380
+        races_thumbnail, races_color = 'https://i.imgur.com/DspJLUH.png', 0x14328C
+        points_thumbnail, points_color = 'https://i.imgur.com/Xm0VNQV.png', 0x0B4A9E
+        awards_thumbnail, awards_color = 'https://i.imgur.com/W9NEYb2.png', 0x0E61D1
+        records_held_thumbnail, records_held_color = 'https://i.imgur.com/3gJNZRO.png', 0x0790E8
+        last_updated_color = 0x00BFFF
+
         faq_information = self.records_information['information']
         faq_embed = discord.Embed(**faq_information)
         for field in faq_information['fields']:
             faq_embed.add_field(**field)
+        faq_embed.set_thumbnail(url = faq_thumbnail)
         faq_embed.set_footer(**faq_information['footer'])
 
         all_records_information = self.records_information['all_records']
 
         speed_information = all_records_information['speed']['records']
         speed_embed = discord.Embed(title = 'Speed Records',
-                                    color = discord.Color(0))
+                                    color = discord.Color(speed_color))
         for speed_record in speed_information:
             speed_embed.add_field(**self.record_field_constructor(speed_record, ' WPM'))
+        speed_embed.set_thumbnail(url = speed_thumbnail)
 
         three_hundred_information = all_records_information['300_wpm_club']['records']
         description, members_list = '', []
@@ -216,18 +229,18 @@ class TypeRacerOnly(commands.Cog):
         members_list = sorted(members_list, key = lambda x: x[1], reverse = True)
 
         for i, member in enumerate(members_list):
-            if i == 0: self.tally(member[0])
             if i < 3: rank = self.medals[i]
             else: rank = f"{i + 1}."
             if member[1] >= 400: wpm = f"**{member[1]}**"
             else: wpm = member[1]
-            description += f"{rank} [{self.get_flag(member[0])} {member[0]} - "
-            description += f"{wpm} WPM]({member[2]}) - {member[3]}\n"
+            description += f"{rank} {self.get_flag(member[0])} {member[0]} - "
+            description += f"[{wpm} WPM]({member[2]}) - {member[3]}\n"
 
         description = description[:-1]
         three_hundred_embed = discord.Embed(title = '300 WPM Club',
-                                            color = discord.Color(0),
+                                            color = discord.Color(three_hundred_color),
                                             description = description)
+        three_hundred_embed.set_thumbnail(url = three_hundred_thumbnail)
         three_hundred_embed.set_footer(text = 'All speeds measured according to adjusted metric')
 
         all_time_leaders = [user for category in self.races_alltime.values() for user in category]
@@ -239,6 +252,8 @@ class TypeRacerOnly(commands.Cog):
         url_constructor = Urls()
         right_now = datetime.datetime.utcnow().timestamp()
         texts_data = load_texts_json()
+        with open(COUNTRY_CODES, 'r') as jsonfile:
+            country_codes = json.load(jsonfile)
         conn = sqlite3.connect(DATABASE_PATH)
         c = conn.cursor()
 
@@ -247,6 +262,7 @@ class TypeRacerOnly(commands.Cog):
                 response = (await fetch([url_constructor.user(user, 'play')], 'text'))[0]
                 medals = {1: 0, 2: 0, 3: 0}
                 soup = BeautifulSoup(response, 'html.parser')
+
                 rows = soup.select("table[class='personalInfoTable']")[0].select('tr')
 
                 for row in rows:
@@ -303,16 +319,16 @@ class TypeRacerOnly(commands.Cog):
             args = args[0]
             for key, value in tally_dict.items():
                 try:
-                    rankings[value].append(formatter(key))
+                    rankings[value].append(key)
                 except KeyError:
-                    rankings[value] = [formatter(key)]
+                    rankings[value] = [key]
 
             rankings = [[k, v] for k, v in sorted(rankings.items(), key = lambda x: x[0], reverse = True)][0:3]
             value = ''
             for i, ranking in enumerate(rankings):
                 if i == 0 and args[0]:
                     for user in ranking[1]:
-                        self.tally(user.split(' ')[-1])
+                        self.tally(user)
 
                 if len(args) == 1:
                     optional = ''
@@ -321,12 +337,15 @@ class TypeRacerOnly(commands.Cog):
                         optional = args[1][ranking[1][0]]
                     except KeyError:
                         optional = ''
+
+                ranking[1] = [formatter(j) for j in ranking[1]]
                 value += f"{self.medals[i]} {' / '.join(ranking[1])} - {f'{round(ranking[0]):,}'}{optional}\n"
             return value
 
         races_information = all_records_information['races']['records']
         races_embed = discord.Embed(title = 'Race Records',
-                                    color = discord.Color(0))
+                                    color = discord.Color(races_color))
+        races_embed.set_thumbnail(url = races_thumbnail)
 
         helper_sorter = lambda param: {k: v[param] for k, v in all_time_data.items()}
         helper_flag = lambda x: f"{self.get_flag(x)} {x}"
@@ -340,19 +359,19 @@ class TypeRacerOnly(commands.Cog):
             medal_breakdown = value['medals']
 
             races_daily_average_dict.update({
-                helper_flag(key): f""" ({f"{round(value['races'], 2):,}"} races over {f"{days:,}"} days)"""
+                key: f""" ({f"{round(value['races'], 2):,}"} races over {f"{days:,}"} days)"""
             })
 
             points_daily_average_dict.update({
-                helper_flag(key): f""" ({f"{round(value['points'], 2):,}"} points over {f"{days:,}"} days)"""
+                key: f""" ({f"{round(value['points']):,}"} points over {f"{days:,}"} days)"""
             })
 
             chars_typed_metadata_dict.update({
-                helper_flag(key): f""" ({f"{round(value['chars_typed'] / value['races'], 2):,}"} average chars over {f"{value['races']:,}"} races)"""
+                key: f""" ({f"{round(value['chars_typed'] / value['races'], 2):,}"} average chars over {f"{value['races']:,}"} races)"""
             })
 
             medal_breakdown_dict.update({
-                helper_flag(key): f""" (:first_place: {f"{medal_breakdown[1]:,}"} :second_place: {f"{medal_breakdown[2]:,}"} :third_place: {f"{medal_breakdown[3]:,}"})"""
+                key: f""" (:first_place: {f"{medal_breakdown[1]:,}"} :second_place: {f"{medal_breakdown[2]:,}"} :third_place: {f"{medal_breakdown[3]:,}"})"""
             })
 
         races_embed.add_field(name = 'All-Time Leaders',
@@ -370,7 +389,8 @@ class TypeRacerOnly(commands.Cog):
 
         points_information = all_records_information['points']['records']
         points_embed = discord.Embed(title = 'Point Records',
-                                     color = discord.Color(0))
+                                     color = discord.Color(points_color))
+        points_embed.set_thumbnail(url = points_thumbnail)
 
         points_embed.add_field(name = 'All-Time Leaders',
                                value = helper_formatter(helper_sorter('points'), helper_flag, (True,)),
@@ -383,7 +403,8 @@ class TypeRacerOnly(commands.Cog):
             points_embed.add_field(**self.record_field_constructor(points_record, ''))
 
         awards_embed = discord.Embed(title = 'Awards Records',
-                                     color = discord.Color(0))
+                                     color = discord.Color(awards_color))
+        awards_embed.set_thumbnail(url = awards_thumbnail)
 
         medal_tally = {k: sum(v['medals'].values()) for k, v in all_time_data.items()}
 
@@ -392,17 +413,20 @@ class TypeRacerOnly(commands.Cog):
                                inline = False)
 
         records_held_embed = discord.Embed(title = 'Records Held',
-                                           color = discord.Color(0))
+                                           color = discord.Color(records_held_color))
+        records_held_embed.set_thumbnail(url = records_held_thumbnail)
 
         top_countries_list = [[k, v] for k, v in self.country_tally.items()]
         records_held_embed.add_field(name = 'Top Countries',
-                                     value = helper_formatter(self.country_tally, lambda x: x, (True,)),
+                                     value = helper_formatter(self.country_tally,\
+                                                              lambda x: f"{x} {country_codes[re.findall(':flag.+:', x)[0][5:-1].strip('_').upper()]}",
+                                                              (False,)),
                                      inline = False)
         records_held_embed.add_field(name = 'Top Users',
-                                     value = helper_formatter(self.user_tally, helper_flag, (True,)),
+                                     value = helper_formatter(self.user_tally, helper_flag, (False,)),
                                      inline = False)
 
-        last_updated_embed = discord.Embed(color = discord.Color(0),
+        last_updated_embed = discord.Embed(color = discord.Color(last_updated_color),
                                            description = f"Last updated **{self.last_updated} UTC**")
 
         return {
@@ -418,10 +442,12 @@ class TypeRacerOnly(commands.Cog):
 
     def record_field_constructor(self, record_information, unit):
         user = record_information['user']
+        if 'Race' in record_information['title'].split(' '): emote = ':cinema:'
+        else: emote = TR_INFO
         value = (f"{self.get_flag(user)} {user} - "
                  f"""{f"{record_information['record']:,}"}{unit} - """
                  f"{record_information['date']} "
-                 f"[:cinema:]({record_information['url']})")
+                 f"[{emote}]({record_information['url']})")
 
         self.tally(user)
 
